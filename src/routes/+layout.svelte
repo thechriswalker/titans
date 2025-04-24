6b56bf1e275ec6dea038e77d9815c35723dfcfea
Chris Walker <chris@thechriswalker.net>
2025-04-24 16:54:57 +0100

    fix: add html title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Tiverton Titans Basketball Club</title>
+</svelte:head>
+
 <div class="flex h-screen flex-col justify-start">
 	<main class="max-w-full space-y-4">
 		{@render children()}
