aa3ebe46c8366022dff139d20a1c7e7fe02c2c9c
Chris Walker <chris@thechriswalker.net>
2025-04-24 17:09:16 +0100

    feat: meta og tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,6 @@
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<title>Tiverton Titans Basketball Club</title>
-</svelte:head>
-
 <div class="flex h-screen flex-col justify-start">
 	<main class="max-w-full space-y-4">
 		{@render children()}
